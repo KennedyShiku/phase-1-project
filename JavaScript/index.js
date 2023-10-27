811d8ca28933b9911983dd4e9789def6f138c073
KennedyShiku <kenrichshiku@gmail.com>
2023-10-27 10:18:27 +0300

    Added enter key press event listener and cleaned code

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -5,38 +5,41 @@ const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".w-icon");
 
-  async function checkWeather(city){
-    const response = await fetch(apiUrl + city + `&appid=${api_key}`);
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${api_key}`);
 
-    if(response === 404){
-      document.querySelector(".error").style.display = "block";
-      // document.querySelector(".error").style.display = "block";
-    } else{
-      
-    }
+  if (response === 404) {
+    document.querySelector(".error").style.display = "block";
+  } else {
+  }
 
-    let data = await response.json();
+  let data = await response.json();
 
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "&#176;C";
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "&#176;C";
+  document.querySelector(".humidity").innerHTML = "Humidity: " + data.main.humidity;
+  document.querySelector(".wind").innerHTML = "Wind Speed: " + data.wind.speed;
 
-    if(data.weather[0].main === "Clouds"){
-      weatherIcon.src = "images/clouds.png";
-    } else if(data.weather[0].main === "Clear"){
-      weatherIcon.src = "images/clear.png"
-    } else if(data.weather[0].main === "Rain"){
-      weatherIcon.src = "images/rain.png"
-    } else if(data.weather[0].main === "Drizzle"){
-      weatherIcon.src = "images/drizzle.png"
-    } else if(data.weather[0].main === "Mist"){
-      weatherIcon.src = "images/mist.png"
-    } 
-
-    // document.querySelector(".info-container").computedStyleMap.display = "block";
+  if (data.weather[0].main === "Clouds") {
+    weatherIcon.src = "images/clouds.png";
+  } else if (data.weather[0].main === "Clear") {
+    weatherIcon.src = "images/clear.png";
+  } else if (data.weather[0].main === "Rain") {
+    weatherIcon.src = "images/rain.png";
+  } else if (data.weather[0].main === "Drizzle") {
+    weatherIcon.src = "images/drizzle.png";
+  } else if (data.weather[0].main === "Mist") {
+    weatherIcon.src = "images/mist.png";
+  }
 }
-searchBtn.addEventListener('click', () =>{
+
+searchBtn.addEventListener('click', () => {
   checkWeather(searchBox.value);
-})
+});
 
-
-
+// Add event listener for Enter key press
+searchBox.addEventListener('keydown', (event) => {
+  if (event.key === "Enter") {
+    checkWeather(searchBox.value);
+  }
+});
